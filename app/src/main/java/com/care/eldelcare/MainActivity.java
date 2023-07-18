05e8ffff1d3dd771578d553f87b69a30aba13d5e
Ricardo Marquez <r.marquez18@info.uas.edu.mx>
2023-07-18 14:14:10 -0700

    Second Commit

---
+++ app/src/main/java/com/care/eldelcare/MainActivity.java
@@ -0,0 +1,13 @@
+package com.care.eldelcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
