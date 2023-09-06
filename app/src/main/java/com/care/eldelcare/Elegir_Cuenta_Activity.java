d0cc707530aa4bef2004aa402a02bc3b5bb47cc6
Ricardo Marquez <r.marquez18@info.uas.edu.mx>
2023-09-05 19:02:06 -0700

    Cambios Nuevos en Cuenta

--- app/src/main/java/com/care/eldelcare/Elegir_Cuenta_Activity.java
+++ app/src/main/java/com/care/eldelcare/Elegir_Cuenta_Activity.java
@@ -1,13 +1,34 @@
 package com.care.eldelcare;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Elegir_Cuenta_Activity extends AppCompatActivity {
+    private ImageView imageViewAsistente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elegir_cuenta);
+        ActionBar actionBar = getActionBar();
+        actionBar.setTitle("Elegir Cuenta");
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.azul)));
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        imageViewAsistente = (ImageView) findViewById(R.id.img_asistente);
+
+        imageViewAsistente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registrarEmpleado = new Intent(Elegir_Cuenta_Activity.this, Registrar_Empleado_Activity.class);
+                startActivity(registrarEmpleado);
+            }
+        });
     }
 }
--- app/src/main/java/com/care/eldelcare/Registrar_Empleado_Activity.java
+++ app/src/main/java/com/care/eldelcare/Registrar_Empleado_Activity.java
@@ -1,7 +1,12 @@
 package com.care.eldelcare;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Registrar_Empleado_Activity extends AppCompatActivity {
 
